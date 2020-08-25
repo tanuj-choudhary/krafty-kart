a82fc9ef1e7a76cd5bf7c7236dc2be2c4bf904da
Tanuj <tanujchoudhary001@gmail.com>
2020-08-25 22:06:22 +0530

    Added most rated items to Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../header/Header';
 import Hero from '../hero/Hero';
 import NewArrivals from '../newArrivals/NewArrivals';
 import ProductGallery from '../productGallery/ProductGallery';
+import MostRatedItems from '../mostRatedItems/MostRatedItems';
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <Hero />
       <NewArrivals />
       <ProductGallery />
+      <MostRatedItems />
     </div>
   );
 }
