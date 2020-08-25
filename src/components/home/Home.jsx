cd6a8ee606c17c277b336ba51d8eef104df155b0
Tanuj <tanujchoudhary001@gmail.com>
2020-08-25 16:57:13 +0530

    Added new Arrivals to Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../header/Header';
 import Hero from '../hero/Hero';
+import NewArrivals from '../newArrivals/NewArrivals';
 
 function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <NewArrivals />
     </div>
   );
 }
