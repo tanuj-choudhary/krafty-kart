1476f91b84c0268744731c55e9816542ab816693
Tanuj <tanujchoudhary001@gmail.com>
2020-08-25 15:34:58 +0530

    Added hero to Home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../header/Header';
+import Hero from '../hero/Hero';
 
 function Home() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
 }
 
 export default Home;
