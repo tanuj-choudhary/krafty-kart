1bd2cf2c3fee0582e9e827e5d6517faa9bbaf939
Tanuj <tanujchoudhary001@gmail.com>
2020-08-26 10:38:03 +0530

    Added footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import FooterBlock from './FooterBlock';
+
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <Container className="footer-container pb-70">
+      <Row className="d-flex justify-content-between mx-5 mx-md-0">
+        <Col xl={4} lg={4} md={6}>
+          <FooterBlock
+            para="aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd."
+            title="Free Shipping Method"
+            icon="fas fa-box"
+          />
+        </Col>
+        <Col xl={4} lg={4} md={6}>
+          <FooterBlock
+            para="aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd."
+            title="Secure Payment System"
+            icon="fas fa-unlock"
+          />
+        </Col>
+        <Col xl={4} lg={4} md={6}>
+          <FooterBlock
+            title="End to End Encryption"
+            para="aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd."
+            icon="fas fa-user-secret"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
