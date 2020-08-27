de7c49b2cbdde31392d934eca3ed0cee465e3de3
Tanuj <tanujchoudhary001@gmail.com>
2020-08-27 08:37:56 +0530

    Added header and footer outside route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Project imports
+import Header from '../components/header/Header';
 import Home from '../components/home/Home';
+import Account from '../components/account/Account';
+import Footer from '../components/footer/Footer';
 
 function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/account/:id" exact component={Account} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
