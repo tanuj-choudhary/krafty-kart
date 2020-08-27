04a430d63c41c8a500224867b65e07b14d150801
Tanuj <tanujchoudhary001@gmail.com>
2020-08-27 08:37:56 +0530

    Added header and footer outside route

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import Header from '../header/Header';
+
 import Hero from '../hero/Hero';
 import NewArrivals from '../newArrivals/NewArrivals';
 import ProductGallery from '../productGallery/ProductGallery';
 import MostRatedItems from '../mostRatedItems/MostRatedItems';
-import Footer from '../footer/Footer';
 
 function Home() {
   return (
     <div>
-      <Header />
       <Hero />
       <NewArrivals />
       <ProductGallery />
       <MostRatedItems />
-      <Footer />
     </div>
   );
 }
