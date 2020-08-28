ae32447ceeede62acc6077980a869a210dd6d8f4
Tanuj <tanujchoudhary001@gmail.com>
2020-08-28 22:24:21 +0530

    Added products page to routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,6 +7,7 @@ import Header from '../components/header/Header';
 import Home from '../components/home/Home';
 import Account from '../components/account/Account';
 import Footer from '../components/footer/Footer';
+import Products from '../components/products/Products';
 
 function Routes() {
   return (
@@ -15,6 +16,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/account/:id" exact component={Account} />
+        <Route path="/products" exact component={Products} />
       </Switch>
       <Footer />
     </BrowserRouter>
