4c13181eeccbc33a82d7a878c03074613585de3f
Tanuj <tanujchoudhary001@gmail.com>
2020-08-29 19:20:39 +0530

    Added product component

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ProductDetail from './ProductDetail';
+
+import './product.scss';
+
+export default function Product() {
+  return (
+    <div>
+      <ProductDetail />
+    </div>
+  );
+}
