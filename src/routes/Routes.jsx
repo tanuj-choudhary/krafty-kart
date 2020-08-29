03f08ce46dd5fffa14b330ad7e1ef62dc29ac1d9
Tanuj <tanujchoudhary001@gmail.com>
2020-08-29 19:21:14 +0530

    Added single product route to routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -8,6 +8,7 @@ import Home from '../components/home/Home';
 import Account from '../components/account/Account';
 import Footer from '../components/footer/Footer';
 import Products from '../components/products/Products';
+import Product from '../components/product/Product';
 
 function Routes() {
   return (
@@ -17,6 +18,7 @@ function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/account/:id" exact component={Account} />
         <Route path="/products" exact component={Products} />
+        <Route path="/product/:id" exact component={Product} />
       </Switch>
       <Footer />
     </BrowserRouter>
