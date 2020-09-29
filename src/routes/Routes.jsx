79d50db58172fd99392bab6e11abd76050753be9
Tanuj <tanujchoudhary001@gmail.com>
2020-09-29 15:12:31 +0530

    Added about page to routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,6 +9,7 @@ import Account from '../components/account/Account';
 import Footer from '../components/footer/Footer';
 import Products from '../components/products/Products';
 import Product from '../components/product/Product';
+import About from '../components/about/About';
 
 function Routes() {
   return (
@@ -19,6 +20,7 @@ function Routes() {
         <Route path="/account/:id" exact component={Account} />
         <Route path="/products" exact component={Products} />
         <Route path="/product/:id" exact component={Product} />
+        <Route path="/about" exact component={About} />
       </Switch>
       <Footer />
     </BrowserRouter>
