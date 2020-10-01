6a2d111271c82b590fd44dbf4a71c5d49374448e
Tanuj <tanujchoudhary001@gmail.com>
2020-10-01 10:45:34 +0530

    Added signup page to route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import Footer from '../components/footer/Footer';
 import Products from '../components/products/Products';
 import Product from '../components/product/Product';
 import About from '../components/about/About';
+import Signup from '../components/signup/Signup';
 
 function Routes() {
   return (
@@ -21,6 +22,7 @@ function Routes() {
         <Route path="/products" exact component={Products} />
         <Route path="/product/:id" exact component={Product} />
         <Route path="/about" exact component={About} />
+        <Route path="/signup" exact component={Signup} />
       </Switch>
       <Footer />
     </BrowserRouter>
