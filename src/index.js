a75da109e16aa94bde4d9361126ae4d26d392d7a
Tanuj <tanujchoudhary001@gmail.com>
2020-10-02 18:50:29 +0530

    Configured redux store, redux thunk and redux devtools and

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 // Third party imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './components/app/app.scss';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 // Project imports
 import App from './components/app/App';
+import reducer from './components/reducer/reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './components/app/app.scss';
+
+// const enhancer =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// const store = createStore(reducer, applyMiddleware(thunk));
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
+
+const store = createStore(reducer, enhancer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
