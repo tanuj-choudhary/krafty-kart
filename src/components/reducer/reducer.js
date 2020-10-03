bb19469d234a37b0c1482c812101bbe3d872c403
Tanuj <tanujchoudhary001@gmail.com>
2020-10-03 12:29:28 +0530

    Added router to root reducer

--- src/components/reducer/reducer.js
+++ src/components/reducer/reducer.js
@@ -1,11 +1,18 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { connectRouter } from 'connected-react-router';
 
 import signupReducer from '../signup/reducer';
+import loginReducer from '../login/reducer';
 import loadingReducer from './loadingReducer';
 
-export default combineReducers({
-  form: formReducer,
-  signup: signupReducer,
-  loadingReducer,
-});
+const createRootReducer = (history) =>
+  combineReducers({
+    form: formReducer,
+    signup: signupReducer,
+    login: loginReducer,
+    loadingReducer,
+    router: connectRouter(history),
+  });
+
+export default createRootReducer;
