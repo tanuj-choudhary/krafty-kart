597f5247432d6f1d5af65988d3c9f0d88b61a204
Tanuj <tanujchoudhary001@gmail.com>
2020-10-03 12:29:06 +0530

    Using store in root file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,14 @@
 // Third party imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 // Project imports
+import store from './utils/store';
 import App from './components/app/App';
-import reducer from './components/reducer/reducer';
 
 import './components/app/app.scss';
 
-// const enhancer =
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-// const store = createStore(reducer, applyMiddleware(thunk));
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
-
-const enhancer = composeEnhancers(
-  applyMiddleware(thunk)
-  // other store enhancers if any
-);
-
-const store = createStore(reducer, enhancer);
 ReactDOM.render(
   <Provider store={store}>
     <App />
