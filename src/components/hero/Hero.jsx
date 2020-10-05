91e23210a1e32cad1421ebdd33e88c2e8ae2d03a
Tanuj <tanujchoudhary001@gmail.com>
2020-10-05 13:59:04 +0530

    Made hero button to link

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,15 @@
+// Third Party Imports
 import React from 'react';
+import { Jumbotron, Container, Col, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-import { Jumbotron, Button, Container, Col, Row } from 'react-bootstrap';
-
+// Project Imports
 import './hero.scss';
 import watch from '../../images/watch.png';
 
-export default function Hero() {
+function Hero() {
+  const productID = '5f7abd72dcc9900ff8dcc3a5';
+
   return (
     <Container className="hero-container" fluid="xs">
       <Jumbotron className=" pt-38 pl-22 pt-md-0 pr-md-0 pb-md-0 mb-0">
@@ -16,11 +20,14 @@ export default function Hero() {
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat is aute irure.
             </p>
-            <p>
-              <Button className="py-25 px-29" variant="primary">
-                SHOP NOW
-              </Button>
-            </p>
+
+            <Link
+              to={{ pathname: `/products/${productID}` }}
+              className="hero-btn py-20 px-35"
+              variant="primary"
+            >
+              SHOP NOW
+            </Link>
           </Col>
           <Col md={4} lg={3}>
             <img src={watch} alt="watch" />
@@ -30,3 +37,4 @@ export default function Hero() {
     </Container>
   );
 }
+export default Hero;
