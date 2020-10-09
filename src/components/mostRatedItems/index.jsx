e5d9a545e049ed62ccf034077cdb601c28efc77d
Tanuj <tanujchoudhary001@gmail.com>
2020-10-09 14:46:50 +0530

    Added most rated items index file

---
+++ src/components/mostRatedItems/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import MostRatedItemsContainer from './MostRatedItemsContainer';
+
+function index() {
+  return <MostRatedItemsContainer />;
+}
+
+export default index;
