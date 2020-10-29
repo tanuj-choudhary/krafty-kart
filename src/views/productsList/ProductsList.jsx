b55815f39be63c10cf32096f40073cf3deea07db
Tanuj <tanujchoudhary001@gmail.com>
2020-10-29 12:05:21 +0530

    Refactored all products page

--- src/views/productsList/ProductsList.jsx
+++ src/views/productsList/ProductsList.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Spinner } from 'react-bootstrap';
 
-import ProductCard from '../productCard/ProductCard';
+import ProductCard from "../productCard";
 
 export default function ProductsList({ products, xs, md, lg }) {
   function renderProductList() {
-    if (!products) {
-      return <div>Loading...</div>;
+    if (!products || products.length === 0) {
+      return (
+        <div className="custom__center">
+          <Spinner variant="primary" animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      );
     }
-
+    
     if (products[0] === 'failed') {
-      return <div>Fail to load products</div>;
+      return <h1 className="custom__center" >Fail to load products</h1>;
     }
 
     return products.map((product) => (
