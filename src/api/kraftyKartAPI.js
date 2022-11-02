09d712b230c1809bc59b58e52cfd3654e2477943
Tanuj Choudhary <51848337+tanuj-choudhary@users.noreply.github.com>
2022-11-02 19:29:12 +0530

    Update kraftyKartAPI.js

--- src/api/kraftyKartAPI.js
+++ src/api/kraftyKartAPI.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export default axios.create({ baseURL: 'http://krafty-kart.herokuapp.com/api/v1' });
+export default axios.create({ baseURL: 'https://krafty-kart.herokuapp.com/api/v1' });
